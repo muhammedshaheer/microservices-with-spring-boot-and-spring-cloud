fbd75527b682d818ba67a71326240636b8d32a05
Muhammed Shaheer <ckshaheer2012@gmail.com>
2021-01-20 16:20:36 +0530

    added logging filter in gateway

---
+++ api-gateway/src/main/java/com/github/muhammedshaheer/apigateway/middleware/LoggingFilter.java
@@ -0,0 +1,26 @@
+package com.github.muhammedshaheer.apigateway.middleware;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author Muhammed Shaheer
+ * @since 20 January 2021
+ */
+
+@Component
+public class LoggingFilter implements GlobalFilter {
+    private static final Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        logger.info("Path of the request received -> {}", exchange.getRequest().getPath());
+        return chain.filter(exchange);
+    }
+}
