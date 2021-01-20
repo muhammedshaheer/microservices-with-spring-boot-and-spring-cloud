ceac94bc80fbdafa5bab25ed006c30dcd746ff9f
Muhammed Shaheer <ckshaheer2012@gmail.com>
2021-01-20 13:05:02 +0530

    added custom gateway and routing

--- api-gateway/src/main/java/com/github/muhammedshaheer/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/github/muhammedshaheer/apigateway/ApiGatewayApplication.java
@@ -3,11 +3,17 @@ package com.github.muhammedshaheer.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * @author Muhammed Shaheer
+ * @since 20 January 2021
+ */
+
 @SpringBootApplication
 public class ApiGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
 
 }
---
+++ api-gateway/src/main/java/com/github/muhammedshaheer/apigateway/configuration/ApiGatewayConfiguration.java
@@ -0,0 +1,40 @@
+package com.github.muhammedshaheer.apigateway.configuration;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Muhammed Shaheer
+ * @since 20 January 2021
+ */
+
+@Configuration
+public class ApiGatewayConfiguration {
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(p -> p.path("/get")
+                        .filters(filter -> filter.addRequestHeader("MyHeader", "MyURI")
+                                .addRequestParameter("Param", "MyValue")
+                        )
+                        .uri("http://httpbin.org:80")
+                )
+                .route(p -> p.path("/currency-exchange/**")
+                        .uri("lb://currency-exchange-service")
+                )
+                .route(p -> p.path("/currency-conversion/**")
+                        .uri("lb://currency-conversion-service")
+                )
+                .route(p -> p.path("/currency-conversion-feign/**")
+                        .uri("lb://currency-conversion-service")
+                )
+                .route(p -> p.path("/currency-conversion-new/**")
+                        .filters(filter -> filter.rewritePath("/currency-conversion-new/", "/currency-conversion-feign/"))
+                        .uri("lb://currency-conversion-service")
+                )
+                .build();
+    }
+}
